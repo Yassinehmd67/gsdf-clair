9407c5cb46047b93388c90187ec4c10312c93cbf
Yassinehmd67 <@example.com>
2026-05-10 09:30:38 +0000

    Initial website upload

---
+++ js/config.js
@@ -0,0 +1,9 @@
+// GSDF CLAIR CONFIG
+
+const COMPANY = {
+  name: "GSDF CLAIR",
+  whatsapp: "212680170327",
+  whatsappLink: "https://wa.me/212680170327",
+  facebookGroup: "https://www.facebook.com/share/g/18Jy6RYm2A/",
+  appName: "IQAMATI",
+};
